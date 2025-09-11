f904795045d55d98894cd882fc3bdace5a709471
Oskar Hokkanen Eriksson <hokkanen.oskar@gmail.com>
2025-09-11 16:20:05 +0200

    More tests

--- src/ArUco/ArUco.jsx
+++ src/ArUco/ArUco.jsx
@@ -1,6 +1,6 @@
 export default function ArUco({ arUcoId }) {
     // Markers generated with https://chev.me/arucogen/
     return (
-        <img src={`../public/4x4_1000-${arUcoId}.svg`} />
+        <img src={`/4x4_1000-${arUcoId}.svg`} />
     )
 }
